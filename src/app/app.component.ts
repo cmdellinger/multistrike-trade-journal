1f50f5a5ab92d5301ef0036fcc6a545f3db748ea
Charles M Dellinger <charles.dellinger@gmail.com>
2024-07-03 11:26:47 -0400

    installed angular material and execucted schematics
    install schematics for:
    - navigation = navigation
    - table = all_trade
    - dashboard = dashboard
    
    linked using angular router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavigationComponent, DashboardComponent]
 })
 export class AppComponent {
   title = 'multistrike-journal';
+
+  logIn() {
+    console.log('testing');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AllTradesComponent } from './all-trades/all-trades.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'all_trades', component: AllTradesComponent },
+    {  path: '**', redirectTo: '/dashboard' }
+];
